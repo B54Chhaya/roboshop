9e3d3f7c1c4bdec28fca59dfc8af93ad10f5e9e2
Chhaya <Chhaya@DESKTOP-NLQC1BR.(none)>
2023-06-11 20:15:52 +0530

    Add code

--- robo/frontend.sh
+++ robo/frontend.sh
@@ -6,18 +6,18 @@ Service=nginx
 
 ID=$(id -u)
 
-#source robo/common.sh
+source robo/common.sh
 
-LOGFILE="/tmp/${COMPONENT}.log"
+#LOGFILE="/tmp/${COMPONENT}.log"
 # Status function to check wheather it success or failure
-stat()  {
-        if [ $1 -eq 0 ] ; then
-            echo -e "\e[33m success \e[0m"
-         else   
-          echo -e  "\e[31m failure \e[0m"
-          exit 2
-         fi
-       }
+#stat()  {
+#       if [ $1 -eq 0 ] ; then
+#            echo -e "\e[33m success \e[0m"
+#         else   
+#         echo -e  "\e[31m failure \e[0m"
+#          exit 2
+#         fi
+#      }
 
 
 echo -e "\e[35m This scripting is for Frontend \e[0m"
