83fbb22ab6f6d02977e8cf466e6a1313bdd05d2c
Chhaya <Chhaya@DESKTOP-NLQC1BR.(none)>
2023-06-11 21:41:01 +0530

    Add code

--- robo/frontend.sh
+++ robo/frontend.sh
@@ -61,7 +61,7 @@ mv localhost.conf /etc/${Service}/default.d/roboshop.conf
 stat $?
 
 echo -n "Updating the reverse proxy details:"
-for component in catalogue user cart shipping payment ; do
+for component in catalogue user cart ; do
     sed -i -e "/$component/s/localhost/$component.roboshop.online/"  /etc/${Service}/default.d/roboshop.conf
 done
 stat $?
