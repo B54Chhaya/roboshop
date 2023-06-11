7380ce0b1ef6c5896a8fddc36c08889a88544486
Chhaya <Chhaya@DESKTOP-NLQC1BR.(none)>
2023-06-11 20:21:47 +0530

    Add code

--- robo/common.sh
+++ robo/common.sh
@@ -2,6 +2,16 @@
 
 LOGFILE="/tmp/${COMPONENT}.log"
 
+ID=$(id -u)
+
+if [ $ID -ne 0 ] ; then   
+echo -e "\e[31m This script will be excuted by Root user or Privileged user \e[0m"
+exit 1
+else
+echo -e "\e[32m User is Root - $ID \e[0m"
+fi
+
+
 # Status function to check wheather it success or failure
 stat()  {
          if [ $1 -eq 0 ] ; then
