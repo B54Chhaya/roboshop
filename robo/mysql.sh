f33aaf94849ff437c65a534d6c42c35c7bd6010d
Chhaya <Chhaya@DESKTOP-NLQC1BR.(none)>
2023-06-12 12:46:38 +0530

    Add code

---
+++ robo/mysql.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+COMPONENT=mysql
+Service=nginx
+ID=$(id -u)
+appuser=roboshop
+
+LOGFILE="/tmp/${COMPONENT}.log"
+# Status function to check wheather it success or failure
+stat()  {
+        if [ $1 -eq 0 ] ; then
+            echo -e "\e[33m success \e[0m"
+         else   
+          echo -e  "\e[31m failure \e[0m"
+          exit 2
+         fi
+       }
+
+echo -e "\e[35m This scripting is for $COMPONENT  \e[0m"
+
+echo -n "To check whether user is root or not "
+if [ $ID -ne 0 ] ; then   
+echo -e "\e[31m This script will be excuted by Root user or Privileged user \e[0m"
+exit 1
+else
+echo -e "\e[32m User is Root - $ID \e[0m"
+fi
+
+echo -e "**************** \e[35m $COMPONENT Installation is started \e[0m **************"
+
+echo -e "Setup $COMPONENT database :"
+curl -s -L -o /etc/yum.repos.d/$COMPONENT.repo https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/${COMPONEN}T.repo &>> $LOGFILE
+stat $?
+
+echo -e "Install $COMPONENT database :"
+curl yum install $COMPONENT-community-server -y &>> $LOGFILE
+stat $?
+
+echo -e "Starting $COMPONENT database :"
+systemctl daemon-reload   &>> $LOGFILE
+systemctl enable mysqld &>> $LOGFILE
+systemctl restart mysqld &>> $LOGFILE
+stat $?
+
+
