08edefcd048d79e2f7c143f857596f97ad34d84c
Chhaya <Chhaya@DESKTOP-NLQC1BR.(none)>
2023-06-12 13:48:04 +0530

    Add code

--- robo/mysql.sh
+++ robo/mysql.sh
@@ -47,12 +47,37 @@ DEFAULT_ROOT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{pr
 stat $?
 
 echo "show databases;" | mysql -uroot -pRoboShop@1 &>> $LOGFILE
-stat $?
 if [ $? -ne 0 ] ; then 
 echo -n "Change default password :"
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" | mysql --connect-expired-password -uroot -p${DEFAULT_ROOT_PASSWORD} | &>> $LOGFILE
 stat $?
 fi
+
+echo "show plugins;" | mysql -uroot -pRoboShop@1 | grep validate_password &>> $LOGFILE
+if [ $? -eq 0 ] ; then 
+    echo -n "Uninstalling the validate_password plugin :"
+    echo "UNINSTALL PLUGIN validate_password;" | mysql -uroot -pRoboShop@1   &>> $LOGFILE
+    stat $?
+fi 
+
+echo -n "Downloading the $COMPONENT schema:"
+curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+stat $? 
+
+echo -n "Extracting the $COMPONENT Schema:"
+cd /tmp  
+unzip -o /tmp/${COMPONENT}.zip   &>> $LOGFILE
+stat $? 
+
+echo -n "Injecting the $COMPONENT Schema :"
+cd ${COMPONENT}-main 
+mysql -u root -pRoboShop@1 <shipping.sql &>> $LOGFILE
+stat $? 
+
+
+echo -e "*********** \e[35m $COMPONENT Installation has Completed \e[0m ***********"
+
+
 
 
 
