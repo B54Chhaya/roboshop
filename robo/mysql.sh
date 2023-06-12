7e468521674d9cde6d115194e8fa030c62550d88
Chhaya <Chhaya@DESKTOP-NLQC1BR.(none)>
2023-06-12 13:25:01 +0530

    Add code

--- robo/mysql.sh
+++ robo/mysql.sh
@@ -47,6 +47,8 @@ DEFAULT_ROOT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{pr
 stat $?
 
 echo -n "Change default password :"
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" | mysql --connect-expired-password -uroot -p${DEFAULT_ROOT_PASSWORD} |
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" | mysql --connect-expired-password -uroot -p${DEFAULT_ROOT_PASSWORD} | &>> $LOGFILE
 stat $?
+
+
 
